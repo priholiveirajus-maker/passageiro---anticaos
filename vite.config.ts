ea78b105f77c5fef638ab2ccd92f34a82a3280fe
priholiveirajus-maker <priholiveirajus@gmail.com>
2026-02-15 23:26:40 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,10 @@
-import path from 'path';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  base: '/passageiro---anticaos/',
+})import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
